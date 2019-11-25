04ed1fe465b600eb819f0ee89f46ee2123e20908
Danielgomes10 <danielgomesdanzinho@gmail.com>
2019-11-25 00:21:52 -0300

    atualizar

--- projeto/app.py
+++ projeto/app.py
@@ -16,9 +16,9 @@ class Pessoa(db.Model):
     cpf = db.Column(db.String)
     email = db.Column(db.String)
 
-def __init_(self, nome, telefone, cpf, email):
+def __init__(self, nome, telefone, cpf, email):
     self.nome = nome
-    self.telefone = telefone
+    self. telefone = telefone
     self.cpf = cpf
     self.email = email
 
@@ -27,6 +27,28 @@ db.create_all()
 @app.route("/index")
 def index():
     return render_template("index.html")
+@app.route("/cadastrar")
+def cadastrar():
+    return render_template("cadastro.html")
+@app.route("/cadastro", methods=['GET', 'POST'])
+def cadastro():
+    if request.method == "POST":
+        nome = request.form.get("nome")
+        telefone = request.form.get("telefone")
+        cpf = request.form.get("cpf")
+        email = request.form.get("email")
+        
+        if nome and telefone and cpf and email:
+            p = Pessoa(nome, telefone, cpf, email)
+            db.session.add(p)
+            db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route("/lista")
+def lista():
+    pessoas = Pessoa.query.all()
+    return render_template("lista.html", pessoas=pessoas)
+
 
 if __name__ == '__main__':
     app.run(debug=True)    
